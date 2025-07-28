36d251c236346c06c9b192c55216ec3c8aab389b
bigdiesel001 <michael.r.collins2022@gmail.com>
2025-07-28 15:20:25 -0500

    Necessary changes so the meme images moves around instead of cat head image

--- eventImage.js
+++ eventImage.js
@@ -1,25 +1,24 @@
 // Get references to DOM elements
 var startBtn = document.getElementById('startBtn');
 var stopBtn = document.getElementById('stopBtn');
+var memeImg = document.getElementById('scienceJokeImage');
 var catImg = document.getElementById('goofyCatImage');
-var memeImg = document.getElementById('scienceMemeImage');
 var moveInterval = null;
 
 
-// This hides the meme image and shows the cat image before clicking the Start button
+// This hides the goofy cat image and shows the science joke image before clicking the Start button
 goofyCatImage.style.display = "block";
 scienceJokeImage.style.display = "none";
 
 
-
 // Function to move the meme image randomly
-function moveCat() {
+function moveMeme() {
     var maxX = window.innerWidth - memeImg.width - 20;
     var maxY = window.innerHeight - memeImg.height - 20;
     var x = Math.random() * maxX;
     var y = Math.random() * maxY;
     memeImg.style.left = x + 'px';
-    memeImg.style.top = y + 'px';    
+    memeImg.style.top = y + 'px';
 }
 
 // Function to enable/disable Start button and start moving
@@ -27,7 +26,7 @@ function enableStart(disable) {
     startBtn.disabled = disable;
     stopBtn.disabled = !disable;
     if (!disable) {
-        moveInterval = setInterval(moveCat, 500);
+        moveInterval = setInterval(moveMeme, 500);
     }
 }
 
@@ -41,24 +40,20 @@ function enableStop(disable) {
     }
 }
 
-// Wire up buttons using onclick properties (not addEventListener) & hides cat image. It also plays the music in the background
+// Wire up buttons using onclick properties (not addEventListener)
 startBtn.onclick = function() {
     enableStart(true);
     goofyCatImage.style.display = "none";
     scienceJokeImage.style.display = "block";
     document.getElementById("goofyMusic").play();
-    moveInterval = setInterval(moveCat, 500);
+    moveInterval = setInterval(moveMeme, 500);
 };
-
-//stops the meme image moving and hides it; it restores the cat image. It also stops the music.
-// It also resets the music to the start.
 stopBtn.onclick = function() {
     enableStop(true);
     goofyCatImage.style.display = "block";
     scienceJokeImage.style.display = "none";
     document.getElementById("goofyMusic").pause();
     document.getElementById("goofyMusic").currentTime = 0; // Reset music to start
-
     if (moveInterval) {
         clearInterval(moveInterval);
         moveInterval = null;
