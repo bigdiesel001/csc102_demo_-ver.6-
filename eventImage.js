9fc5125d6540a1554ec801bf598037814d8ec0b0
bigdiesel001 <michael.r.collins2022@gmail.com>
2025-07-28 15:09:44 -0500

    Make a quick change to have the meme image move around instead of the cat image

--- eventImage.js
+++ eventImage.js
@@ -6,20 +6,20 @@ var memeImg = document.getElementById('scienceMemeImage');
 var moveInterval = null;
 
 
-// This hides the goofy cat image and shows the science joke image before clicking the Start button
-goofyCatImage.style.display = "none";
-scienceJokeImage.style.display = "block";
+// This hides the meme image and shows the cat image before clicking the Start button
+goofyCatImage.style.display = "block";
+scienceJokeImage.style.display = "none";
 
 
 
-// Function to move the cat image randomly
+// Function to move the meme image randomly
 function moveCat() {
-    var maxX = window.innerWidth - catImg.width - 20;
-    var maxY = window.innerHeight - catImg.height - 20;
+    var maxX = window.innerWidth - memeImg.width - 20;
+    var maxY = window.innerHeight - memeImg.height - 20;
     var x = Math.random() * maxX;
     var y = Math.random() * maxY;
-    catImg.style.left = x + 'px';
-    catImg.style.top = y + 'px';    
+    memeImg.style.left = x + 'px';
+    memeImg.style.top = y + 'px';    
 }
 
 // Function to enable/disable Start button and start moving
@@ -41,21 +41,21 @@ function enableStop(disable) {
     }
 }
 
-// Wire up buttons using onclick properties (not addEventListener) & hides meme image. It also plays the music in the background
+// Wire up buttons using onclick properties (not addEventListener) & hides cat image. It also plays the music in the background
 startBtn.onclick = function() {
     enableStart(true);
-    goofyCatImage.style.display = "block";
-    scienceJokeImage.style.display = "none";
+    goofyCatImage.style.display = "none";
+    scienceJokeImage.style.display = "block";
     document.getElementById("goofyMusic").play();
     moveInterval = setInterval(moveCat, 500);
 };
 
-//stops the cat image moving and hides it; it restores the meme image. It also stops the music.
+//stops the meme image moving and hides it; it restores the cat image. It also stops the music.
 // It also resets the music to the start.
 stopBtn.onclick = function() {
     enableStop(true);
-    goofyCatImage.style.display = "none";
-    scienceJokeImage.style.display = "block";
+    goofyCatImage.style.display = "block";
+    scienceJokeImage.style.display = "none";
     document.getElementById("goofyMusic").pause();
     document.getElementById("goofyMusic").currentTime = 0; // Reset music to start
 
